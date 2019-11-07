db8ec5a9f845f05211471d4afa3aa641c404d6fa
Reece Mercer <reecemercer981@gmail.com>
2019-11-07 15:11:23 +0000

    Added first class and created project structure

---
+++ src/vote.cpp
@@ -0,0 +1,5 @@
+//
+// Created by reece on 07/11/2019.
+//
+
+#include "vote.h"
---
+++ src/vote.h
@@ -0,0 +1,15 @@
+//
+// Created by reece on 07/11/2019.
+//
+
+#ifndef CPP_COURSEWORK_VOTE_H
+#define CPP_COURSEWORK_VOTE_H
+
+typedef unsigned int candidate;
+
+class vote {
+
+};
+
+
+#endif //UNTITLED1_VOTE_H
